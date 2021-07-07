74eded61e4174c1970965fe1baaff809202cd1e7
olivia <zhouxuanolivia@gmail.com>
2021-07-07 22:45:33 +1000

    finialise all the details

--- SortNames.cs
+++ SortNames.cs
@@ -5,8 +5,10 @@ using System.Linq;
 
 
 /* 
-Sort all the last names. If all the last names different with each other, the sorting is done.
-If repetitations exist, treat all the first name(s) as a string. Compare the first_name strings.
+This class helps to sort names from a given file. The main features are as follows:
+1.Load names data from a file. 
+2.Sort the names in descending order. The priority to compare is firstly last name then first names.
+3.Export the sorted output to sorted-names-list.txt.
 */
 namespace GlobalX
 {
@@ -111,43 +113,15 @@ namespace GlobalX
             return last_names;
         }
 
-        public static string[] GetFirstNames(List<String> unsorted_names)
-        {
-            int size = GetSize(unsorted_names);
-            string[] first_names = new string[size];
-
-
-            for (int i = 0; i < size; i++)
-            {
-                string full_name = unsorted_names[i];
-                int indexOfLastEmptySpace = full_name.LastIndexOf(" ");
-                string first_name = full_name.Substring(0, indexOfLastEmptySpace);
-                // System.Console.WriteLine(first_name);
-                first_names[i] = first_name;
-            }
-            System.Console.WriteLine("****************************************************************s");
-            foreach (String name in first_names)
-            {
-                System.Console.WriteLine(name);
-            }
-            return first_names;
-        }
-
-
+        // Get the first name based on the given full name
         public static string GetFirstName(string full_name)
         {
             int indexOfLastEmptySpace = full_name.LastIndexOf(" ");
             return full_name.Substring(0, indexOfLastEmptySpace);
         }
 
-        public static string GetFullName(string first_name)
-        {
-            string full_name = "";
-            return full_name;
-        }
-
-
-        public static string[] GetSortedNames(string[] last_names, List<String> unsorted_names)
+        // Sort and return the names according to last names
+        public static string[] SortByLastName(string[] last_names, List<String> unsorted_names)
         {
             (List<String> sorted_last_names, List<int> sorted__last_index) = Sorting(last_names);
 
@@ -163,6 +137,43 @@ namespace GlobalX
             return sorted_names;
         }
 
+        // Sort the names by first names. Treat all the first names as a whole string.
+        public static string[] SortByFirstName(string[] last_names, List<String> unsorted_names, string[] sorted_names)
+        {
+            (List<String> sorted_last_names, List<int> sorted__last_index) = Sorting(last_names);
+            
+            // If duplicated last names exist, compare them related first names
+            String[] distinct_last_names = GetDistinctNames(last_names);
+            foreach (String name in distinct_last_names)
+            {
+                // Find the old indexes and new indexes of duplicated last names in arraies.
+                int[] new_indexes = FindIndexes(sorted_last_names.ToArray(), name);
+                int[] old_indexes = FindIndexes(last_names, name);
+
+                List<String> first_names = new List<String>();
+                Dictionary<string, string> name_dict = new Dictionary<string, string>();
+
+                // Add the first name string to an array
+                foreach (int index in old_indexes)
+                {
+                    string first_name = GetFirstName(unsorted_names[index]);
+                    string full_name = unsorted_names[index];
+                    first_names.Add(first_name);
+                    name_dict.Add(first_name, full_name);
+                }
+                string[] unsorted_first_names = first_names.ToArray();
+
+                //Compare the first name strings, according to the order, update corresponding full name to the correct position
+                (List<String> sorted_first_names, List<int> sorted_first_index) = Sorting(unsorted_first_names);
+                for (int i = 0; i < old_indexes.Length; i++)
+                {
+                    string full_name = name_dict[sorted_first_names[i]];
+                    sorted_names[new_indexes[i]] = full_name;
+                }
+            }
+            return sorted_names;
+        }
+
         static void Main(string[] args)
         {
             StreamReader file = ValidateInput(args[0], args[1]);
@@ -172,78 +183,12 @@ namespace GlobalX
 
             // Get all last names as an array, sort them
             string[] last_names = GetLastNames(unsorted_names);
+            string[] sorted_names = SortByLastName(last_names, unsorted_names);
 
-            (List<String> sorted_last_names, List<int> sorted__last_index) = Sorting(last_names);
+            // Sort by the first names
+            sorted_names = SortByFirstName(last_names, unsorted_names, sorted_names);
 
-            string[] sorted_names = GetSortedNames(last_names, unsorted_names);
-
-
-
-
-            String[] distinct_last_names = GetDistinctNames(last_names);
-
-            foreach (String name in distinct_last_names)
-            {
-                // Find the nex indexes and old indexes of duplicated last names in arraies.
-                int[] new_indexes = FindIndexes(sorted_last_names.ToArray(), name);
-                int[] old_indexes = FindIndexes(last_names, name);
-
-                // List<String> full_names = new List<String>();
-                // foreach (int index in old_indexes)
-                // {
-                //     full_names.Add(unsorted_names[index]);
-                // }
-                // string[] unsorted_first_names = full_names.ToArray();
-
-                List<String> first_names = new List<String>();
-                Dictionary<string, string> name_dict = new Dictionary<string, string>();
-
-                foreach (int index in old_indexes)
-                {
-                    string first_name = GetFirstName(unsorted_names[index]);
-                    string full_name = unsorted_names[index];
-                    first_names.Add(first_name);
-                    name_dict.Add(first_name, full_name);
-                    // full_names.Add(full_name);
-                    // System.Console.WriteLine(full_name);
-                }
-                string[] unsorted_first_names = first_names.ToArray();
-                // string[] unsorted_full_names = full_names.ToArray();
-
-                System.Console.WriteLine("****************************************************************");
-                foreach (String n in unsorted_first_names)
-                {
-                    System.Console.WriteLine(n);
-                }
-
-                System.Console.WriteLine("@@@@@@@@@@@@@@@@");
-                // string[] unsorted_first_names = GetFirstNames(unsorted_names);
-                (List<String> sorted_first_names, List<int> sorted_first_index) = Sorting(unsorted_first_names);
-                foreach (string n in sorted_first_names)
-                {
-                    System.Console.WriteLine(n);
-                }
-
-                foreach (int n in sorted_first_index)
-                {
-                    System.Console.WriteLine(n);
-                }
-
-                for (int i = 0; i < old_indexes.Length; i++)
-                {
-                    // sorted_names[new_indexes[i]] = sorted_first_names[i];
-                    string full_name = name_dict[sorted_first_names[i]];
-                    sorted_names[new_indexes[i]] = full_name;
-                }
-
-                // String[] new_names = GetFirstNames(unsorted_names);
-            }
-
-
-            System.Console.WriteLine("================================================================");
             PrintResults(sorted_names);
-
-
         }
     }
 }
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -28,14 +28,31 @@ namespace Tests
             var results2 = SortNames.GetDistinctNames(list2);
             String[] expected2 = { "Lindsey", "Lopez" };
             Assert.AreEqual(expected2, results2);
+
+            string[] list3 = { "Lindsey", "Lopez", "Clarke" };
+            var results3 = SortNames.GetDistinctNames(list3);
+            String[] expected3 = { };
+            Assert.AreEqual(expected3, results3);
         }
+
 
         [Test]
         public void TestGetSize()
         {
             var list = new List<string> { "London Lindsey", "Mia Banena Lopez", "Hunter Uriah Mathew Clarke" };
             var results = SortNames.GetSize(list);
-            int expected = 3;
+            var expected = 3;
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void TestSorting()
+        {
+            string[] list = { "Lindsey", "Banena", "Apple" };
+            var results = SortNames.Sorting(list);
+            List<String> sorted_names = new List<string> { "Apple", "Banena", "Lindsey" };
+            List<int> sorted_index = new List<int> { 2, 1, 0 };
+            var expected = Tuple.Create(sorted_names, sorted_index);
             Assert.AreEqual(expected, results);
         }
 
@@ -49,5 +66,45 @@ namespace Tests
             Assert.AreEqual(expected, results);
         }
 
+        [Test]
+        public void TestGetFistName()
+        {
+            var results = SortNames.GetFirstName("Beau Tristan Bentley");
+            string expected = "Beau Tristan";
+            Assert.AreEqual(expected, results);
+
+            var results2 = SortNames.GetFirstName("Vaughn Lewis");
+            string expected2 = "Vaughn";
+            Assert.AreEqual(expected2, results2);
+        }
+
+        [Test]
+        public void TestSortByLastName()
+        {
+            var unsorted_names = new List<string> { "Adonis Julius Archer", "Marin Alvarez",
+            "Janet Parsons","Hunter Uriah Mathew Clarke"};
+            string[] last_names = { "Archer", "Alvarez", "Parsons", "Clarke" };
+            var results = SortNames.SortByLastName(last_names, unsorted_names);
+            string[] expected = { "Marin Alvarez", "Adonis Julius Archer", "Hunter Uriah Mathew Clarke", "Janet Parsons" };
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void TestSortByFirstName()
+        {
+            var unsorted_names = new List<string> { "B Parsons", "C Parsons", "A Parsons" };
+            string[] last_names = SortNames.GetLastNames(unsorted_names);
+            string[] sorted_names = SortNames.SortByLastName(last_names, unsorted_names);
+            var results = SortNames.SortByFirstName(last_names, unsorted_names, sorted_names);
+            string[] expected = { "A Parsons", "B Parsons", "C Parsons" };
+            Assert.AreEqual(expected, results);
+
+            var unsorted_names2 = new List<string> { "B X Parsons", "B Parsons", "C Parsons", "A Parsons" };
+            string[] last_names2 = SortNames.GetLastNames(unsorted_names2);
+            string[] sorted_names2 = SortNames.SortByLastName(last_names2, unsorted_names2);
+            var results2 = SortNames.SortByFirstName(last_names2, unsorted_names2, sorted_names2);
+            string[] expected2 = { "A Parsons", "B Parsons", "B X Parsons", "C Parsons" };
+            Assert.AreEqual(expected2, results2);
+        }
     }
 }
