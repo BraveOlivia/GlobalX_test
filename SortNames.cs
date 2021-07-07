23f3aa49ff4cd8d5544d610e87878b89aa4d3778
olivia <zhouxuanolivia@gmail.com>
2021-07-07 20:55:02 +1000

    solve compare several first names bug

--- SortNames.cs
+++ SortNames.cs
@@ -111,6 +111,42 @@ namespace GlobalX
             return last_names;
         }
 
+        public static string[] GetFirstNames(List<String> unsorted_names)
+        {
+            int size = GetSize(unsorted_names);
+            string[] first_names = new string[size];
+
+
+            for (int i = 0; i < size; i++)
+            {
+                string full_name = unsorted_names[i];
+                int indexOfLastEmptySpace = full_name.LastIndexOf(" ");
+                string first_name = full_name.Substring(0, indexOfLastEmptySpace);
+                // System.Console.WriteLine(first_name);
+                first_names[i] = first_name;
+            }
+            System.Console.WriteLine("****************************************************************s");
+            foreach (String name in first_names)
+            {
+                System.Console.WriteLine(name);
+            }
+            return first_names;
+        }
+
+
+        public static string GetFirstName(string full_name)
+        {
+            int indexOfLastEmptySpace = full_name.LastIndexOf(" ");
+            return full_name.Substring(0, indexOfLastEmptySpace);
+        }
+
+        public static string GetFullName(string first_name)
+        {
+            string full_name = "";
+            return full_name;
+        }
+
+
         public static string[] GetSortedNames(string[] last_names, List<String> unsorted_names)
         {
             (List<String> sorted_last_names, List<int> sorted__last_index) = Sorting(last_names);
@@ -152,22 +188,62 @@ namespace GlobalX
                 int[] new_indexes = FindIndexes(sorted_last_names.ToArray(), name);
                 int[] old_indexes = FindIndexes(last_names, name);
 
-                List<String> full_names = new List<String>();
+                // List<String> full_names = new List<String>();
+                // foreach (int index in old_indexes)
+                // {
+                //     full_names.Add(unsorted_names[index]);
+                // }
+                // string[] unsorted_first_names = full_names.ToArray();
+
+                List<String> first_names = new List<String>();
+                Dictionary<string, string> name_dict = new Dictionary<string, string>();
+
                 foreach (int index in old_indexes)
                 {
-                    full_names.Add(unsorted_names[index]);
+                    string first_name = GetFirstName(unsorted_names[index]);
+                    string full_name = unsorted_names[index];
+                    first_names.Add(first_name);
+                    name_dict.Add(first_name, full_name);
+                    // full_names.Add(full_name);
+                    // System.Console.WriteLine(full_name);
                 }
-                string[] unsorted_first_names = full_names.ToArray();
+                string[] unsorted_first_names = first_names.ToArray();
+                // string[] unsorted_full_names = full_names.ToArray();
 
+                System.Console.WriteLine("****************************************************************");
+                foreach (String n in unsorted_first_names)
+                {
+                    System.Console.WriteLine(n);
+                }
+
+                System.Console.WriteLine("@@@@@@@@@@@@@@@@");
+                // string[] unsorted_first_names = GetFirstNames(unsorted_names);
                 (List<String> sorted_first_names, List<int> sorted_first_index) = Sorting(unsorted_first_names);
+                foreach (string n in sorted_first_names)
+                {
+                    System.Console.WriteLine(n);
+                }
+
+                foreach (int n in sorted_first_index)
+                {
+                    System.Console.WriteLine(n);
+                }
 
                 for (int i = 0; i < old_indexes.Length; i++)
                 {
-                    sorted_names[new_indexes[i]] = sorted_first_names[i];
+                    // sorted_names[new_indexes[i]] = sorted_first_names[i];
+                    string full_name = name_dict[sorted_first_names[i]];
+                    sorted_names[new_indexes[i]] = full_name;
                 }
+
+                // String[] new_names = GetFirstNames(unsorted_names);
             }
 
+
+            System.Console.WriteLine("================================================================");
             PrintResults(sorted_names);
+
+
         }
     }
 }
