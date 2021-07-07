f5d1bc82334915ddb63b95cef9a66e4728b2330c
olivia <zhouxuanolivia@gmail.com>
2021-07-07 18:26:14 +1000

    first nunit test

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Tests
@@ -10,9 +12,23 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void TestGetLastNames()
         {
-            Assert.Pass();
+            List<String> unsorted_names = new List<String>();
+            unsorted_names.Add("London Lindsey");
+            unsorted_names.Add("Mia Banena Lopez");
+            unsorted_names.Add("Hunter Uriah Mathew Clarke");
+            var results = GlobalX.SortNames.GetLastNames(3, unsorted_names);
+
+            string[] expected = { "Lindsey", "Lopez", "Clarke" };
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void TestGetIndexes()
+        {
+            // string[] expected = { "Lindsey", "Lopez", "Clarke" };
+            Assert.AreEqual(1, 1);
         }
     }
 }
